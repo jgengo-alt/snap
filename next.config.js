b1d5ee631cd54db5b386be46c0490b78dadd23d7
Jordane Gengo <jordane@hive.fi>
2022-04-25 19:19:50 +0300

    add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  reactStrictMode: true,
+  optimizeFonts: false,
+  assetPrefix: isProd ? "/snap/" : "",
+};
+
+module.exports = nextConfig;
