6698de72d3eb048d1f07eab2db7cdf0eb61ee57c
jgengo <jgengo@users.noreply.github.com>
2022-04-26 10:45:34 +0000

    deploy: eec03e9153f861ec97803734a1a9067cce48ada2

--- _next/static/ZOd9FMyMfnw752YEnz8r6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d788a696fd3d1617.js"],"/_error":["static/chunks/pages/_error-68c8fb2ff52d9081.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/dPk7c5WIwNdcVclvzO8Vm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a96424f77f2047f1.js"],"/_error":["static/chunks/pages/_error-68c8fb2ff52d9081.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
